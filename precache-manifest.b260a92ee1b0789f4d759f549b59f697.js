ab98c7ff8d2170556f516979f2abafebea5fe696
Ammko128 <ammar.veljagic@gmail.com>
2020-09-18 02:11:05 +0200

    Updates

---
+++ precache-manifest.b260a92ee1b0789f4d759f549b59f697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7159e4f95a36141e21dcdf4731aa06",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "27ba881489a3c994e774",
+    "url": "/calculator/static/css/main.008ef171.chunk.css"
+  },
+  {
+    "revision": "7f9879fe6087ea3c1968",
+    "url": "/calculator/static/js/2.d201be8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.d201be8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ba881489a3c994e774",
+    "url": "/calculator/static/js/main.b7dfaf58.chunk.js"
+  },
+  {
+    "revision": "ee4a148f714da062db4f",
+    "url": "/calculator/static/js/runtime-main.f0d0de70.js"
+  }
+]);
